cda4b7ed9b93dd0afd41029ecc6741e6652fa5eb
homosapian <ankitd349@gmail.com>
2023-10-01 12:40:39 +0530

    jj

--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ dotev.config();
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.json("Hello Ankit Dubey. This is testing of the github stuff");
+  res.json(
+    "Hello Ankit Chandrasagar Babita Dubey. This is testing of the github stuff"
+  );
 });
 
 app.listen(PORT, () => {
